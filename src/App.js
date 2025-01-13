d5501e818c5b4dc92849fc5cccb178e7ffd0d101
madhu.97mca <madhu.97mca@gmail.com>
2025-01-13 13:03:22 +0530

    updating App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          This is Demo Project for Docker
-        </p>
+         <br>
+         This is Demo Project for Docker Tutotial !!!
+         </br>         
+         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
